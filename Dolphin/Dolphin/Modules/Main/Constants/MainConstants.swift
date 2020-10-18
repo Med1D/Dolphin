4bb9254ede2cbb294e03c28d9fb641c73d18f0c0
MED1D <medvedev.id@bk.ru>
2020-10-18 23:51:05 +0300

    (D-25) Add table view and custom cell

---
+++ Dolphin/Dolphin/Modules/Main/Constants/MainConstants.swift
@@ -0,0 +1,22 @@
+//
+//  MainConstants.swift
+//  Dolphin
+//
+//  Created by Иван Медведев on 18.10.2020.
+//
+
+import UIKit
+
+enum MainConstants
+{
+	static let helveticaNeue14 = UIFont(name: "HelveticaNeue", size: 14)
+	static let helveticaNeueLight14 = UIFont(name: "HelveticaNeue-Light", size: 14)
+	static let helveticaNeueMedium16 = UIFont(name: "HelveticaNeue-Medium", size: 16)
+	static let imageSize: CGFloat = 64
+	static let imageTopOffset: CGFloat = 8
+	static let imageBottomOffset: CGFloat = 8
+	static let imageLeadingOffset: CGFloat = 16
+	static let messageTimeOffset: CGFloat = 16
+	static let messageAndNameTrailingOffset: CGFloat = 64
+	static let highlightedCellColor = UIColor(red: 52 / 255, green: 70 / 255, blue: 93 / 255, alpha: 1)
+}
